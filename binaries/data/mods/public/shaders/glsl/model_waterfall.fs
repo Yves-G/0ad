3de5b6ae9adae9013e59e92e45a1daffdefa19e2
Yves <yves.gwerder@wildfiregames.com>
2016-06-25 22:35:23 +0200

    Port the model_waterfall shader to the GL4 branch.

--- binaries/data/mods/public/shaders/glsl/model_waterfall.fs
+++ binaries/data/mods/public/shaders/glsl/model_waterfall.fs
@@ -1,38 +1,109 @@
-#version 120
+#version 430
+#extension GL_ARB_bindless_texture : require
 
-uniform sampler2D baseTex;
-uniform sampler2D losTex;
+in VS_OUT
+{
+  flat uint drawID;
+} fs_in;
 
+out vec4 fragColor;
 
-#if USE_SHADOW
+layout(shared) buffer FrameUBO
+{
+	vec4 sim_time;
+
+	mat4 transform;
+	vec3 cameraPos;
+
+	mat4 shadowTransform;
+	vec4 shadowScale;
+
+	vec3 ambient;	// only used in fragment shader
+	vec3 sunColor;
+	vec3 sunDir;
+
+	vec3 fogColor;	// only used in fragment shader
+	vec2 fogParams;	// only used in fragment shader
+
+	vec2 losTransform;
+	layout (bindless_sampler) sampler2D losTex;
+
+// TODO: It has to be ensured that all blocks in all shaders are the same
+// (they must not have different defines that cause a difference)
+
   #if USE_SHADOW_SAMPLER
-    uniform sampler2DShadow shadowTex;
-    #if USE_SHADOW_PCF
-      uniform vec4 shadowScale;
-    #endif
+    layout (bindless_sampler) sampler2DShadow shadowTex;
   #else
-    uniform sampler2D shadowTex;
+    layout (bindless_sampler) sampler2D shadowTex;
   #endif
-#endif
 
-uniform vec3 shadingColor;
-uniform vec3 ambient;
-uniform vec3 sunColor;
-uniform vec3 sunDir;
-uniform vec3 cameraPos;
+};
 
+// TODO: make block members conditional again
+struct DrawStruct
+{
+  uint modelId;
+  mat4 instancingTransform;
+};
 
-uniform float specularPower;
-uniform vec3 specularColor;
+layout(shared) buffer DrawBlock
+{
+  DrawStruct draws[];
+};
 
+struct ModelStruct
+{
+  uint matId;
+  vec4 playerColor;
+  vec3 shadingColor;
+};
 
-varying vec4 v_tex;
-varying vec4 v_shadow;
-varying vec2 v_los;
-varying vec3 v_half;
-varying vec3 v_normal;
-varying float v_transp;
-varying vec3 v_lighting;
+layout(shared) buffer ModelBlock
+{
+  ModelStruct models[];
+};
+
+struct MaterialStruct
+{
+  uint templateMatId;
+  vec3 objectColor;
+  layout (bindless_sampler) sampler2D baseTex;
+  layout (bindless_sampler) sampler2D aoTex;
+  layout (bindless_sampler) sampler2D normTex;
+  layout (bindless_sampler) sampler2D specTex;
+};
+
+layout(shared) buffer MaterialUBO
+{
+  MaterialStruct material[];
+};
+
+struct MatTemplStruct
+{
+//#if USE_SPECULAR
+  float specularPower;
+  vec3 specularColor;
+//#endif
+
+//#if USE_NORMAL_MAP || USE_SPECULAR_MAP || USE_PARALLAX || USE_AO
+  vec4 effectSettings;
+//#endif
+
+  vec4 windData;
+};
+
+layout(shared) buffer MatTemplBlock
+{
+  MatTemplStruct matTempl[];
+};
+
+in vec4 v_tex;
+in vec4 v_shadow;
+in vec2 v_los;
+in vec3 v_half;
+in vec3 v_normal;
+in float v_transp;
+in vec3 v_lighting;
 
 float get_shadow()
 {
@@ -43,17 +114,17 @@ float get_shadow()
         vec4 size = vec4(offset + 1.0, 2.0 - offset);
         vec4 weight = (vec4(1.0, 1.0, -0.5, -0.5) + (v_shadow.xy - 0.5*offset).xyxy) * shadowScale.zwzw;
         return (1.0/9.0)*dot(size.zxzx*size.wwyy,
-          vec4(shadow2D(shadowTex, vec3(weight.zw, v_shadow.z)).r,
-               shadow2D(shadowTex, vec3(weight.xw, v_shadow.z)).r,
-               shadow2D(shadowTex, vec3(weight.zy, v_shadow.z)).r,
-               shadow2D(shadowTex, vec3(weight.xy, v_shadow.z)).r));
+          vec4(texture(shadowTex, vec3(weight.zw, v_shadow.z)).r,
+               texture(shadowTex, vec3(weight.xw, v_shadow.z)).r,
+               texture(shadowTex, vec3(weight.zy, v_shadow.z)).r,
+               texture(shadowTex, vec3(weight.xy, v_shadow.z)).r));
       #else
-        return shadow2D(shadowTex, v_shadow.xyz).r;
+        return texture(shadowTex, v_shadow.xyz).r;
       #endif
     #else
       if (v_shadow.z >= 1.0)
         return 1.0;
-      return (v_shadow.z <= texture2D(shadowTex, v_shadow.xy).x ? 1.0 : 0.0);
+      return (v_shadow.z <= texture(shadowTex, v_shadow.xy).x ? 1.0 : 0.0);
     #endif
   #else
     return 1.0;
@@ -63,26 +134,30 @@ float get_shadow()
 
 void main()
 {
-	//vec4 texdiffuse = textureGrad(baseTex, vec3(fract(v_tex.xy), v_tex.z), dFdx(v_tex.xy), dFdy(v_tex.xy));
-	vec4 texdiffuse = texture2D(baseTex, fract(v_tex.xy));
+	const uint modelIdVal = draws[fs_in.drawID].modelId;
+	const uint materialIDVal = models[modelIdVal].matId;
+	const uint matTemplIdVal = material[materialIDVal].templateMatId;
+
+	//vec4 texdiffuse = textureGrad(material[materialIDVal].baseTex, vec3(fract(v_tex.xy), v_tex.z), dFdx(v_tex.xy), dFdy(v_tex.xy));
+	vec4 texdiffuse = texture(material[materialIDVal].baseTex, fract(v_tex.xy));
 
 	if (texdiffuse.a < 0.25)
 		discard;
 
 	texdiffuse.a *= v_transp;
 
-	vec3 specular = sunColor * specularColor * pow(max(0.0, dot(normalize(v_normal), v_half)), specularPower);
+	vec3 specular = sunColor * matTempl[matTemplIdVal].specularColor * pow(max(0.0, dot(normalize(v_normal), v_half)), matTempl[matTemplIdVal].specularPower);
 
 	vec3 color = (texdiffuse.rgb * v_lighting + specular) * get_shadow();
         color += texdiffuse.rgb * ambient;
 
 	#if !IGNORE_LOS
-		float los = texture2D(losTex, v_los).a;
+		float los = texture(losTex, v_los).a;
 		los = los < 0.03 ? 0.0 : los;
 		color *= los;
 	#endif
 
-	gl_FragColor.rgb = color;
-	gl_FragColor.a = texdiffuse.a;
+	fragColor.rgb = color;
+	fragColor.a = texdiffuse.a;
 }
 
--- binaries/data/mods/public/shaders/glsl/model_waterfall.vs
+++ binaries/data/mods/public/shaders/glsl/model_waterfall.vs
@@ -1,46 +1,127 @@
-#if USE_GPU_SKINNING
-// Skinning requires GLSL 1.30 for ivec4 vertex attributes
-#version 130
-#else
-#version 120
-#endif
+#version 430
+#extension GL_ARB_bindless_texture : require
 
-uniform mat4 transform;
-uniform vec3 cameraPos;
-uniform vec3 sunDir;
-uniform vec3 sunColor;
-uniform vec2 losTransform;
-uniform mat4 shadowTransform;
-uniform mat4 instancingTransform;
+in uint a_drawId;
 
-uniform float sim_time;
-uniform vec2 translation;
+out VS_OUT
+{
+    flat uint drawID;
+} vs_out;
 
-#if USE_SHADOW_SAMPLER && USE_SHADOW_PCF
-  uniform vec4 shadowScale;
-#endif
+layout(shared) buffer FrameUBO
+{
+	vec4 sim_time;
 
+	mat4 transform;
+	vec3 cameraPos;
 
-attribute vec3 a_vertex;
-attribute vec3 a_normal;
-attribute vec2 a_uv0;
-attribute vec2 a_uv1;
+	mat4 shadowTransform;
+	vec4 shadowScale;
 
-varying vec4 worldPos;
-varying vec4 v_tex;
-varying vec4 v_shadow;
-varying vec2 v_los;
-varying vec3 v_half;
-varying vec3 v_normal;
-varying float v_transp;
-varying vec3 v_lighting;
+	vec3 ambient;	// only used in fragment shader
+	vec3 sunColor;
+	vec3 sunDir;
+
+	vec3 fogColor;	// only used in fragment shader
+	vec2 fogParams;	// only used in fragment shader
+
+	vec2 losTransform;
+	layout (bindless_sampler) sampler2D losTex;
+
+// TODO: It has to be ensured that all blocks in all shaders are the same
+// (they must not have different defines that cause a difference)
+
+  #if USE_SHADOW_SAMPLER
+    layout (bindless_sampler) sampler2DShadow shadowTex;
+  #else
+    layout (bindless_sampler) sampler2D shadowTex;
+  #endif
+
+};
+
+// TODO: make block members conditional again
+struct DrawStruct
+{
+  uint modelId;
+  mat4 instancingTransform;
+};
+
+layout(shared) buffer DrawBlock
+{
+  DrawStruct draws[];
+};
+
+struct ModelStruct
+{
+  uint matId;
+  vec4 playerColor;
+  vec3 shadingColor;
+};
+
+layout(shared) buffer ModelBlock
+{
+  ModelStruct models[];
+};
+
+struct MaterialStruct
+{
+  uint templateMatId;
+  vec3 objectColor;
+  layout (bindless_sampler) sampler2D baseTex;
+  layout (bindless_sampler) sampler2D aoTex;
+  layout (bindless_sampler) sampler2D normTex;
+  layout (bindless_sampler) sampler2D specTex;
+};
+
+layout(shared) buffer MaterialUBO
+{
+  MaterialStruct material[];
+};
+
+struct MatTemplWaterStruct
+{
+  vec2 translation;
+// TODO: shininess is in the material files but it doesn't seem to be used anywhere
+// shininess
+  float specularStrength;
+  float waviness;
+  vec3 waterTint;
+  float murkiness;
+  vec3 reflectionTint;
+  float reflectionTintStrength;
+};
+
+layout(shared) buffer MatTemplWaterBlock
+{
+  MatTemplWaterStruct matTemplWater[];
+};
+
+in vec3 a_vertex;
+in vec3 a_normal;
+in vec2 a_uv0;
+in vec2 a_uv1;
+
+out vec4 worldPos;
+out vec4 v_tex;
+out vec4 v_shadow;
+out vec2 v_los;
+out vec3 v_half;
+out vec3 v_normal;
+out float v_transp;
+out vec3 v_lighting;
 
 
 void main()
 {
-	worldPos = instancingTransform * vec4(a_vertex, 1.0);
+	const uint modelIdVal = draws[a_drawId].modelId;
+	const uint materialIDVal = models[modelIdVal].matId;
+	const uint matTemplIdVal = material[materialIDVal].templateMatId;
+
+	vs_out.drawID = a_drawId;
+
+	worldPos = draws[a_drawId].instancingTransform * vec4(a_vertex, 1.0);
 	
-	v_tex.xy = a_uv0 + sim_time * translation;
+	v_tex.xy = a_uv0 + sim_time.x * matTemplWater[matTemplIdVal].translation;
 	v_transp = a_uv1.x;
 
 	#if USE_SHADOW
@@ -56,7 +137,7 @@ void main()
         vec3 sunVec = -sunDir;
         v_half = normalize(sunVec + normalize(eyeVec));
 
-	mat3 normalMatrix = mat3(instancingTransform[0].xyz, instancingTransform[1].xyz, instancingTransform[2].xyz);
+	mat3 normalMatrix = mat3(draws[a_drawId].instancingTransform[0].xyz, draws[a_drawId].instancingTransform[1].xyz, draws[a_drawId].instancingTransform[2].xyz);
 	v_normal = normalMatrix * a_normal;
 	v_lighting = max(0.0, dot(v_normal, -sunDir)) * sunColor;
 
